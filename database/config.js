6f9f22ee1d4eb4dd9200b5f34944576645da5ccd
jazz07 <jguzmanm17@miumg.edu.gt>
2022-10-30 11:25:03 -0600

    heroku

--- database/config.js
+++ database/config.js
@@ -3,11 +3,11 @@ config();
 
 module.exports = {
     db: {
-        user: 'root',
-        password: 'root',
-        host: 'localhost',
+        user: 'ikzlfnfinavcat',
+        password: 'e0ee8d511fa81b1e31c1825c43068e0de4b264816b557fc75848b8fe3a07493b',
+        host: 'ec2-3-220-207-90.compute-1.amazonaws.com',
         port: 5432,
-        database: 'cebe',
+        database: 'd45hd1v6900g43',
     }
 }
 
