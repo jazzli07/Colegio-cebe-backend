af88a8fe837f553b61349f744fd4c423d5416f30
jazz07 <jguzmanm17@miumg.edu.gt>
2022-10-30 12:36:11 -0600

    cambioish

--- database/config.js
+++ database/config.js
@@ -3,11 +3,11 @@ config();
 
 module.exports = {
     db: {
-        user: process.env.DB_USER ||'ikzlfnfinavcat',
-        password: process.env.DB_PASSWORD || 'e0ee8d511fa81b1e31c1825c43068e0de4b264816b557fc75848b8fe3a07493b',
-        host: process.env.DB_HOST || 'ec2-3-220-207-90.compute-1.amazonaws.com',
-        port: process.env.DB_PORT || 5432,
-        database: process.env.DB_DATABASE || 'd45hd1v6900g43',
+        user: 'ikzlfnfinavcat',
+        password: 'e0ee8d511fa81b1e31c1825c43068e0de4b264816b557fc75848b8fe3a07493b',
+        host: 'ec2-3-220-207-90.compute-1.amazonaws.com',
+        port: 5432,
+        database: 'd45hd1v6900g43',
     
 }
 }
